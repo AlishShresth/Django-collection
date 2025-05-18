0e7f90cb57dd3cc62259084888e0b650128bc1f3
alishShrestha <alish.shrestha@dolphin.com.np>
2025-05-18 22:55:14 +0545

    feat: Readonly user viewset

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
 from . import views
+from .views import UserViewSet
+
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("", include(router.urls)),
 ]
--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import RegisterSerializer, UserSerializer
-
+from .models import CustomUser
 
 class RegisterView(APIView):
     """API endpoint for user registration."""
@@ -16,3 +17,10 @@ class RegisterView(APIView):
             user = serializer.save()
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """User viewset"""
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
